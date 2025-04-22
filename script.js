70da3ba91f48daa90531329d7dd65437e551d661
buenex <43445161+buenex@users.noreply.github.com>
2025-04-21 23:42:03 -0300

    Update content
    Added some details

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var normalImageElement = document.getElementById("normalImage");
 var shinyImageElement = document.getElementById("shinyImage");
 var nameElement = document.getElementById("pokemonName");
+var pokedexDescriptionElement = document.getElementById("pokedexDescription");
+var cryAudioElement = document.getElementById("cryAudio");
 
 function pokemonNumberDay() {
     const today = new Date();
@@ -13,15 +15,27 @@ function pokemonNumberDay() {
     return pokemonNum;
 }
 
+function getPokedex(pokemonNumber) {
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonNumber}`)
+        .then(response => response.json())
+        .then(data => {
+            pokedexDescriptionElement.innerHTML = data.flavor_text_entries.filter((item)=>item.language.name =="en")[0].flavor_text
+        })
+        .catch(error => console.error("Erro:", error));
+}
+
 function getPokemon(pokemonNumber) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`)
         .then(response => response.json())
         .then(data => {
             normalImageElement.src=data.sprites.other["official-artwork"].front_default
             shinyImageElement.src=data.sprites.other["official-artwork"].front_shiny
-            nameElement.innerHTML = data.name[0].toUpperCase()+data.name.substring(1);
+            nameElement.innerHTML = `#${pokemonNumber} ${data.name[0].toUpperCase()+data.name.substring(1)}`;
+            cryAudioElement.src = data.cries.latest
         })
         .catch(error => console.error("Erro:", error));
 }
 
-getPokemon(pokemonNumberDay())
+const pokemonNumber = pokemonNumberDay()
+getPokemon(pokemonNumber)
+getPokedex(pokemonNumber)
